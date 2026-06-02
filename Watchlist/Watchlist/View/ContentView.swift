38433f8304f98245822d9a0c505c1f43fb2ff089
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-02 13:19:09 -0700

    Update View for add and delete Movie option.

--- Watchlist/Watchlist/View/ContentView.swift
+++ Watchlist/Watchlist/View/ContentView.swift
@@ -12,16 +12,46 @@ struct ContentView: View {
     //MARK: - PROPERTIES
     @Environment(\.modelContext) private var modelContext
     @Query private var movies: [Movie]
+    @State private var isSheetPresented:Bool = false
 
     var body: some View {
         List {
-            
-        }
+            ForEach(movies) { movie in
+                HStack{
+                    Text(movie.title)
+                    Spacer()
+                    Text(movie.genre.name)
+                }//: LIST ROW
+                .swipeActions {
+                    Button(role: .destructive) {
+                        withAnimation {
+                            modelContext.delete(movie)
+                        }
+                    } label: {
+                        Label("Delete",systemImage: "trash")
+                    }
+                }
+            }
+        }//: LIST
         .overlay {
             if movies.isEmpty {
                 EmptyListView()
             }
         }
+        // MARK: - SAFE AREA
+        .safeAreaInset(edge:.bottom,alignment:.center) {
+            //NEW MOVIE BUTTON
+            Button {
+                isSheetPresented.toggle()
+            } label: {
+                ButtonImageView(symbolName: "plus.circle.fill")
+            }
+        }//: SAFE AREA
+        //MARK: - SHEET
+        .sheet(isPresented: $isSheetPresented) {
+          NewMovieFormView()
+        }
+        
     }
 
 //    private func addItem() {
@@ -38,6 +68,10 @@ struct ContentView: View {
 //            }
 //        }
 //    }
+}
+
+#Preview("Sample Data") {
+    ContentView()
 }
 
 #Preview("Empty List") {
