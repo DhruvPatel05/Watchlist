e435e0bc7b68e24252a107382bb42aa696761ba8
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-03 13:03:04 -0700

    Default Movie watchlist updated UI.

--- Watchlist/Watchlist/View/ContentView.swift
+++ Watchlist/Watchlist/View/ContentView.swift
@@ -13,6 +13,13 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var movies: [Movie]
     @State private var isSheetPresented:Bool = false
+    @State private var randomMovie:String = ""
+    @State private var isShowingAlert:Bool = false
+    
+    // MARK: - Functions
+    private func randomMovieGenerator() {
+        randomMovie = movies.randomElement()!.title
+    }
 
     var body: some View {
         List {
@@ -68,12 +75,34 @@ struct ContentView: View {
             }
         }
         // MARK: - SAFE AREA
-        .safeAreaInset(edge:.bottom,alignment:.center) {
-            //NEW MOVIE BUTTON
-            Button {
-                isSheetPresented.toggle()
-            } label: {
-                ButtonImageView(symbolName: "plus.circle.fill")
+        .safeAreaInset(edge:.bottom,alignment:.center)  {
+            HStack {
+                if movies.count >= 2 {
+                    // RANDOMIZE BUTTON
+                    Button {
+                        randomMovieGenerator()
+                        isShowingAlert = true
+                    }  label: {
+                        ButtonImageView(symbolName: "arrow.trianglehead.2.clockwise.rotate.90.circle.fill")
+                    }
+                    .alert(randomMovie, isPresented: $isShowingAlert) {
+                        Button("OK",role: .cancel) {
+                            
+                        }
+                    }
+                    .accessibilityLabel("Random Movie")
+                    .sensoryFeedback(.success, trigger: isShowingAlert)
+                    Spacer()
+                }
+                //NEW MOVIE BUTTON
+                Button {
+                    isSheetPresented.toggle()
+                } label: {
+                    ButtonImageView(symbolName: "plus.circle.fill")
+                }
+                .accessibilityLabel("New Movie")
+                .sensoryFeedback(.success, trigger: isSheetPresented)
+                .padding(.horizontal)
             }
         }//: SAFE AREA
         //MARK: - SHEET
